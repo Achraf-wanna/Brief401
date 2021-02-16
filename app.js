edcc4fbd0261d2f2bac6e9bacba0eb7d6830aa95
Achraf <ninjagoo147@outlook.com>
2021-02-16 16:30:44 +0100

    Done

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+const mongoose = require('mongoose')
+
+
+
+var userRouter = require('./routes/user');
+
+var app = express();
+mongoose.connect( 'mongodb://localhost/brief401' ,{ useNewUrlParser: true , useUnifiedTopology: true },(err)=>{
+  if(err){
+    console.log(err);
+    return
+  }else{
+    console.log('connected')
+  }
+
+})
+
+
+
+app.use(express.json());
+
+app.use('/', userRouter);
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+app.listen(3000)
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
